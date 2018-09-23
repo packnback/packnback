4a31976fbc4b46a1e87e9c952a5efc88947b93e4
Andrew Chambers <andrewchambers@fastmail.com>
2018-09-24 02:09:05 +1200

    Add sha2_256

--- rust/chaclstar/src/lib.rs
+++ rust/chaclstar/src/lib.rs
@@ -1,3 +1,6 @@
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
 pub mod nacl;
+#[allow(non_upper_case_globals)]
+#[allow(non_camel_case_types)]
+pub mod sha2_256;
--- rust/chaclstar/update.sh
+++ rust/chaclstar/update.sh
@@ -38,4 +38,5 @@ cat <<EOF >> build.rs
 EOF
 
 bindgen ./c/haclnacl.h --whitelist-function "crypto_.*" --whitelist-var  "crypto_.*" > src/nacl.rs
+bindgen ./c/Hacl_SHA2_256.h --whitelist-function "Hacl_.*" --whitelist-var  "Hacl_.*" > src/sha2_256.rs
 rustfmt src/nacl.rs
