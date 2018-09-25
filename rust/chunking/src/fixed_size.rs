cbc69fddb80b6fb299984b44bfbaed84f1a81b96
Andrew Chambers <andrewchambers@fastmail.com>
2018-09-25 14:39:32 +1200

    Add efficient chunk reading for io::Read.

--- rust/chunking/src/fixed_size.rs
+++ rust/chunking/src/fixed_size.rs
@@ -3,6 +3,20 @@ pub struct FixedSizeChunker {
     cur_vec: Box<Vec<u8>>,
 }
 
+fn read_exact_or_eof(r: &mut std::io::Read, buf: &mut [u8]) -> Result<usize, std::io::Error> {
+    let mut buf = buf;
+    let mut n: usize = 0;
+    loop {
+        match r.read(buf)? {
+            0 => return Ok(n),
+            n_read => {
+                n += n_read;
+                buf = &mut buf[n_read..];
+            }
+        }
+    }
+}
+
 impl FixedSizeChunker {
     pub fn new(sz: usize) -> FixedSizeChunker {
         assert!(sz != 0);
@@ -28,6 +42,30 @@ impl FixedSizeChunker {
         }
     }
 
+    pub fn read_chunk(
+        &mut self,
+        r: &mut std::io::Read,
+    ) -> Result<Option<Box<Vec<u8>>>, std::io::Error> {
+        let start_len = self.cur_vec.len();
+        // We want to read directly into the current vec to avoid
+        // a copy. This should be ok because we are just growing the vec
+        // to it's actual capacity in anticipation of a read.
+        // On error some uninitialized u8's will be in the trailing end of the buffer,
+        // So on error the chunker should not be reused.
+        unsafe { self.cur_vec.set_len(self.cur_vec.capacity()) };
+        let n_read = read_exact_or_eof(r, &mut self.cur_vec[start_len..])?;
+        self.cur_vec.truncate(start_len + n_read);
+
+        if self.cur_vec.len() == self.sz {
+            let mut v = Box::new(Vec::<u8>::with_capacity(self.sz));
+            std::mem::swap(&mut v, &mut self.cur_vec);
+            assert!(v.len() == self.sz);
+            Ok(Some(v))
+        } else {
+            Ok(None)
+        }
+    }
+
     pub fn finish(self) -> Option<Box<Vec<u8>>> {
         if self.cur_vec.len() == 0 {
             None
@@ -35,5 +73,53 @@ impl FixedSizeChunker {
             assert!(self.cur_vec.len() < self.sz);
             Some(self.cur_vec)
         }
+    }
+}
+
+#[test]
+fn test_add_bytes() {
+    let mut ch = FixedSizeChunker::new(2);
+
+    match ch.add_bytes(b"a") {
+        (1, None) => (),
+        v => panic!("{:?}", v),
+    }
+
+    match ch.add_bytes(b"bc") {
+        (1, Some(v)) => assert_eq!(*v, b"ab"),
+        v => panic!("{:?}", v),
+    }
+
+    match ch.add_bytes(b"c") {
+        (1, None) => (),
+        v => panic!("{:?}", v),
+    }
+
+    match ch.finish() {
+        Some(v) => assert_eq!(*v, b"c"),
+        v => panic!("{:?}", v),
+    }
+}
+
+#[test]
+fn test_read_chunk() {
+    use std::io::Cursor;
+
+    let mut ch = FixedSizeChunker::new(2);
+    let mut cur = Cursor::new(b"abc");
+
+    match ch.read_chunk(&mut cur).unwrap() {
+        Some(v) => assert_eq!(*v, b"ab"),
+        v => panic!("{:?}", v),
+    }
+
+    match ch.read_chunk(&mut cur).unwrap() {
+        None => (),
+        v => panic!("{:?}", v),
+    }
+
+    match ch.finish() {
+        Some(v) => assert_eq!(*v, b"c"),
+        v => panic!("{:?}", v),
     }
 }
